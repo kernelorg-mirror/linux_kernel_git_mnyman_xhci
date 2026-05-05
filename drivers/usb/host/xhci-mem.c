0b57900f01175471810fc61b14bdc0ff1ee25426
Mathias Nyman <mathias.nyman@linux.intel.com>
2026-05-05 22:10:46 +0300

    xhci: fix frame id calculation and checks for isoc URBs
    Check if the expected frame IDs for a isochronous URB submitted
    mid stream is within the valid frame time window that xHC controller
    is capable of queuing TDs.
    
    The range only needs to be checked once per URB as the isoc TDs of an
    URB are queued in one go with spinlock held and interrupts disabled.
    
    Calculate the valid frame window start and end frame id in frames
    instead of microframes to better match how xhci specification
    section 4.11.2.5 does it.
    
    Don't add frame id gaps or change scheduling to SIA mid stream if
    the start frame is outside the valid frame winow.
    Only print a debug message.
    Some devices can't handle gaps in isochronous transfers.
    
    Calculate a valid start frame for the first URB of a stream, and
    align it to a full frame, or to interval start if interval is longer
    than a frame
    
    Set urb->start_frame value for every URB
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>

--- drivers/usb/host/xhci-mem.c
+++ drivers/usb/host/xhci-mem.c
@@ -1499,6 +1499,7 @@ int xhci_endpoint_init(struct xhci_hcd *xhci,
 		return -ENOMEM;
 
 	virt_dev->eps[ep_index].skip = false;
+	virt_dev->eps[ep_index].next_uframe = -1;
 	ep_ring = virt_dev->eps[ep_index].new_ring;
 	xhci_ring_init(xhci, ep_ring);
 
